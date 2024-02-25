b7d7e96067b812fd2a5b1daf59cb93999f9050e8
geethaCH <chgeethakrishna@gmail.com>
2024-02-25 17:24:38 +0530

    added commit to getstudentexams

--- routes/getStudentExams.js
+++ routes/getStudentExams.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { studentExams } from "../controllers/getStudentExams.js";
 const router = express.Router();
+import { validateAccessToken } from "../util/middleware.js";
 
-router.get("/enrollexams", studentExams);
+router.get("/enrollexams", validateAccessToken, studentExams);
 
 export default router;
